440a050e550f35e04765a7956517b5fcbeb93e10
fiveseven-lambda <fiveseven.lambda@gmail.com>
2020-01-21 10:22:28 +0900

    copy, fast/slow

--- a.c
+++ a.c
@@ -1,6 +1,7 @@
 #include <X11/Xlib.h>
 #include <X11/keysym.h>
 #include <stdio.h>
+#include <unistd.h>
 
 #define N 500
 #define in(x, y) ((x) >= 0 && (x) < N && (y) >= 0 && (y) < N)
@@ -49,13 +50,21 @@ int main(){
 	static char field[N][N];
 	static int count[N][N];
 
+	static char copy[N][N];
+	struct{
+		int x1, x2;
+		int y1, y2;
+	} copy_area;
+
+	int sleeptime = 16384;
+
 set:
 	XSelectInput(display, window, ExposureMask | KeyPressMask | ButtonPressMask | ButtonMotionMask | ButtonReleaseMask | StructureNotifyMask);
 	XClearArea(display, window, 0, 0, width, height, True);
 	for(;;){
 		XNextEvent(display, &event);
 		switch(event.type){
-			_Bool no_move, button1pressed;
+			_Bool no_move, button1pressed, copying;
 			struct{
 				int x;
 				int y;
@@ -89,10 +98,42 @@ set:
 				break;
 			case ButtonPress:
 				if(event.xbutton.button == Button1){
-					pressed.x = event.xbutton.x - center.x;
-					pressed.y = event.xbutton.y - center.y;
-					no_move = True;
-					button1pressed = True;
+					if(copying){
+						int x = event.xbutton.x;
+						int y = event.xbutton.y;
+						if(x >= 0) x /= scale;
+						else x = -(-x - 1)/scale - 1;
+						if(y >= 0) y /= scale;
+						else y = -(-y - 1)/scale - 1;
+						for(int i = copy_area.x1; i <= copy_area.x2; ++i){
+							for(int j = copy_area.y1; j <= copy_area.y2; ++j){
+								if(in(i + N / 2, j + N / 2)){
+									set[i + N / 2][j + N / 2] = 0;
+								}
+							}
+						}
+						for(int i = copy_area.x1; i <= copy_area.x2; ++i){
+							for(int j = copy_area.y1; j <= copy_area.y2; ++j){
+								if(in(i + N / 2, j + N / 2) && in(i + x - copy_area.x1 + N / 2, j + y - copy_area.y1 + N / 2)){
+									set[i + x - copy_area.x1 + N / 2][j + y - copy_area.y1 + N / 2]
+										= copy[i + N / 2][j + N / 2];
+								}
+							}
+						}
+						XClearArea(display, window, 0, 0, width, height, True);
+					}else{
+						pressed.x = event.xbutton.x - center.x;
+						pressed.y = event.xbutton.y - center.y;
+						no_move = True;
+						button1pressed = True;
+					}
+				}else if(event.xbutton.button == Button3){
+					copy_area.x1 = event.xbutton.x;
+					copy_area.y1 = event.xbutton.y;
+					if(copy_area.x1 >= 0) copy_area.x1 /= scale;
+					else copy_area.x1 = -(-copy_area.x1 - 1)/scale - 1;
+					if(copy_area.y1 >= 0) copy_area.y1 /= scale;
+					else copy_area.y1 = -(-copy_area.y1 - 1)/scale - 1;
 				}else if(event.xbutton.button == Button4){
 					center.x = event.xbutton.x - (event.xbutton.x - center.x) * (scale + 1) / scale;
 					center.y = event.xbutton.y - (event.xbutton.y - center.y) * (scale + 1) / scale;
@@ -115,17 +156,46 @@ set:
 				break;
 			case ButtonRelease:
 				if(event.xbutton.button == Button1){
-					if(no_move){
-						int i = pressed.x;
-						int j = pressed.y;
-						if(i >= 0) i /= scale;
-						else i = -(-i - 1)/scale - 1;
-						if(j >= 0) j /= scale;
-						else j = -(-j - 1)/scale - 1;
-						if(in(i + N / 2, j + N / 2)) set[i + N / 2][j + N / 2] ^= 1;
-						XClearArea(display, window, center.x + i * scale, center.y + j * scale, scale, scale, True);
+					if(copying){
+						copying = False;
+					}else{
+						if(no_move){
+							int i = pressed.x;
+							int j = pressed.y;
+							if(i >= 0) i /= scale;
+							else i = -(-i - 1)/scale - 1;
+							if(j >= 0) j /= scale;
+							else j = -(-j - 1)/scale - 1;
+							if(in(i + N / 2, j + N / 2)) set[i + N / 2][j + N / 2] ^= 1;
+							XClearArea(display, window, center.x + i * scale, center.y + j * scale, scale, scale, True);
+						}
 					}
 					button1pressed = False;
+				}else if(event.xbutton.button == Button3){
+					copy_area.x2 = event.xbutton.x;
+					copy_area.y2 = event.xbutton.y;
+					if(copy_area.x2 >= 0) copy_area.x2 /= scale;
+					else copy_area.x2 = -(-copy_area.x2 - 1)/scale - 1;
+					if(copy_area.y2 >= 0) copy_area.y2 /= scale;
+					else copy_area.y2 = -(-copy_area.y2 - 1)/scale - 1;
+					if(copy_area.x1 > copy_area.x2){
+						int tmp = copy_area.x1;
+						copy_area.x1 = copy_area.x2;
+						copy_area.x2 = tmp;
+					}
+					if(copy_area.y1 > copy_area.y2){
+						int tmp = copy_area.y1;
+						copy_area.y1 = copy_area.y2;
+						copy_area.y2 = tmp;
+					}
+					for(int i = copy_area.x1; i <= copy_area.x2; ++i){
+						for(int j = copy_area.y1; j <= copy_area.y2; ++j){
+							if(in(i + N / 2, j + N / 2)){
+								copy[i + N / 2][j + N / 2] = set[i + N / 2][j + N / 2];
+							}
+						}
+					}
+					copying = True;
 				}
 				break;
 			case KeyPress:
@@ -134,6 +204,13 @@ set:
 						goto run;
 					case XK_Escape:
 						XDestroyWindow(display, window);
+						break;
+					case 's':
+						sleeptime *= 2;
+						break;
+					case 'f':
+						if(sleeptime > 1) sleeptime /= 2;
+						break;
 				}
 		}
 	}
@@ -161,6 +238,13 @@ run:
 							goto set;
 						case XK_Escape:
 							XDestroyWindow(display, window);
+							break;
+						case 's':
+							sleeptime *= 2;
+							break;
+						case 'f':
+							if(sleeptime > 1) sleeptime /= 2;
+							break;
 					}
 			}
 		}else{
@@ -201,7 +285,7 @@ run:
 						);
 				}
 			}
-			XFlush(display);
+			usleep(sleeptime);
 		}
 	}
 }
