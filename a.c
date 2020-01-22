f1da9d1e046a5ec3356c2707f25c50a21dfe4b64
fiveseven-lambda <fiveseven.lambda@gmail.com>
2020-01-22 15:35:30 +0900

    you can copy & paste

--- a.c
+++ a.c
@@ -4,7 +4,14 @@
 #include <unistd.h>
 
 #define N 500
-#define in(x, y) ((x) >= 0 && (x) < N && (y) >= 0 && (y) < N)
+#define IN(x, a, b) (((a) < (b) && (a) <= (x) && (x) <= (b)) || ((a) >= (b) && (b) <= (x) && (x) <= (a)))
+#define INFIELD(x, y) (IN(x, 0, N) && IN(y, 0, N))
+
+#define RNG(a, b) ((a) < (b) ? (b) - (a) : (a) - (b))
+
+#define DIV(x, y) ((x) >= 0 ? (x) / (y) : -((-(x) - 1) / (y)) - 1)
+
+
 
 int main(){
 	Display *display = XOpenDisplay(NULL);
@@ -28,6 +35,7 @@ int main(){
 		0, white,
 		white
 	);
+	XSelectInput(display, window, ExposureMask | KeyPressMask | ButtonPressMask | PointerMotionMask | ButtonReleaseMask | StructureNotifyMask);
 	XMapWindow(display, window);
 	XEvent event;
 
@@ -38,12 +46,12 @@ int main(){
 	GC whiteGC = XCreateGC(display, window, GCForeground, &xgc);
 
 	long scale = 10;
-	struct{
-		long x;
-		long y;
+	struct point{
+		int x;
+		int y;
 	} center = {
-		.x = 0,
-		.y = 0
+		.x = - N / 2 * scale,
+		.y = - N / 2 * scale
 	};
 
 	static char set[N][N];
@@ -52,38 +60,48 @@ int main(){
 
 	static char copy[N][N];
 	struct{
-		int x1, x2;
-		int y1, y2;
+		int i1, i2;
+		int j1, j2;
 	} copy_area;
+	enum { not_selecting, selecting, selected, pasting } select_mode;
+	struct {
+		int i;
+		int j;
+	} pasting_pointer;
+
+	struct point pressed;
+	_Bool button1pressed = False;
+	_Bool no_move;
 
 	int sleeptime = 16384;
-
 set:
-	XSelectInput(display, window, ExposureMask | KeyPressMask | ButtonPressMask | ButtonMotionMask | ButtonReleaseMask | StructureNotifyMask);
-	XClearArea(display, window, 0, 0, width, height, True);
 	for(;;){
 		XNextEvent(display, &event);
 		switch(event.type){
-			_Bool no_move, button1pressed, copying;
-			struct{
-				int x;
-				int y;
-			} pressed;
 			case Expose:
 				for(
 					int i = (event.xexpose.x - center.x) / scale - 1;
 					i <= (event.xexpose.x - center.x + event.xexpose.width) / scale;
 					++i
+				)
+				for(
+					int j = (event.xexpose.y - center.y) / scale - 1;
+					j <= (event.xexpose.y - center.y + event.xexpose.height) / scale;
+					++j
 				){
-					for(
-						int j = (event.xexpose.y - center.y) / scale - 1;
-						j <= (event.xexpose.y - center.y + event.xexpose.height) / scale;
-						++j
-					){
-						if(in(i + N / 2, j + N / 2) && !set[i + N / 2][j + N / 2]){
-							int size = scale - (scale >= 10);
-							XFillRectangle(display, window, blackGC, center.x + i * scale, center.y + j * scale, size, size);
+					if(INFIELD(i, j)){
+						_Bool flag = !set[i][j];
+						if(select_mode == selecting || select_mode == selected){
+							flag ^= IN(i, copy_area.i1, copy_area.i2) && IN(j, copy_area.j1, copy_area.j2);
+						}else if(
+							select_mode == pasting &&
+							IN(copy_area.i1 + i - pasting_pointer.i, copy_area.i1, copy_area.i2 - 1) &&
+							IN(copy_area.j1 + j - pasting_pointer.j, copy_area.j1, copy_area.j2 - 1)
+						){
+							flag &= !copy[copy_area.i1 + i - pasting_pointer.i][copy_area.j1 + j - pasting_pointer.j];
 						}
+						int size = scale - (scale >= 10);
+						if(flag) XFillRectangle(display, window, blackGC, center.x + i * scale, center.y + j * scale, size, size);
 					}
 				}
 				break;
@@ -98,48 +116,32 @@ set:
 				break;
 			case ButtonPress:
 				if(event.xbutton.button == Button1){
-					if(copying){
-						int x = event.xbutton.x;
-						int y = event.xbutton.y;
-						if(x >= 0) x /= scale;
-						else x = -(-x - 1)/scale - 1;
-						if(y >= 0) y /= scale;
-						else y = -(-y - 1)/scale - 1;
-						for(int i = copy_area.x1; i <= copy_area.x2; ++i){
-							for(int j = copy_area.y1; j <= copy_area.y2; ++j){
-								if(in(i + N / 2, j + N / 2)){
-									set[i + N / 2][j + N / 2] = 0;
-								}
-							}
-						}
-						for(int i = copy_area.x1; i <= copy_area.x2; ++i){
-							for(int j = copy_area.y1; j <= copy_area.y2; ++j){
-								if(in(i + N / 2, j + N / 2) && in(i + x - copy_area.x1 + N / 2, j + y - copy_area.y1 + N / 2)){
-									set[i + x - copy_area.x1 + N / 2][j + y - copy_area.y1 + N / 2]
-										= copy[i + N / 2][j + N / 2];
-								}
-							}
-						}
+					if(event.xbutton.state & ControlMask){
+						// Ctrl を押しながらクリック
+						// 範囲選択を開始
+						copy_area.i1 = copy_area.i2 = DIV(event.xbutton.x - center.x, scale);
+						copy_area.j1 = copy_area.j2 = DIV(event.xbutton.y - center.y, scale);
+						select_mode = selecting;
 						XClearArea(display, window, 0, 0, width, height, True);
+					}else if(select_mode == pasting){
+						// Ctrl+V を押した後のクリック
+						// 貼り付け　または　移動
+						no_move = True;
 					}else{
+						// 白マス/黒マス切り替え　または　移動
 						pressed.x = event.xbutton.x - center.x;
 						pressed.y = event.xbutton.y - center.y;
 						no_move = True;
-						button1pressed = True;
 					}
-				}else if(event.xbutton.button == Button3){
-					copy_area.x1 = event.xbutton.x;
-					copy_area.y1 = event.xbutton.y;
-					if(copy_area.x1 >= 0) copy_area.x1 /= scale;
-					else copy_area.x1 = -(-copy_area.x1 - 1)/scale - 1;
-					if(copy_area.y1 >= 0) copy_area.y1 /= scale;
-					else copy_area.y1 = -(-copy_area.y1 - 1)/scale - 1;
+					button1pressed = True;
 				}else if(event.xbutton.button == Button4){
+					// 拡大 (今ポインタがある場所を中心に)
 					center.x = event.xbutton.x - (event.xbutton.x - center.x) * (scale + 1) / scale;
 					center.y = event.xbutton.y - (event.xbutton.y - center.y) * (scale + 1) / scale;
 					++scale;
 					XClearArea(display, window, 0, 0, width, height, True);
 				}else if(event.xbutton.button == Button5 && scale > 1){
+					// 縮小 (今ポインタがある場所を中心に)
 					center.x = event.xbutton.x - (event.xbutton.x - center.x) * (scale - 1) / scale;
 					center.y = event.xbutton.y - (event.xbutton.y - center.y) * (scale - 1) / scale;
 					--scale;
@@ -148,68 +150,96 @@ set:
 				break;
 			case MotionNotify:
 				if(button1pressed){
-					center.x = event.xmotion.x - pressed.x;
-					center.y = event.xmotion.y - pressed.y;
-					no_move = False;
+					// ドラッグ中に動いた
+					if(select_mode == selecting){
+						// 範囲選択
+						copy_area.i2 = DIV(event.xmotion.x - center.x, scale);
+						copy_area.j2 = DIV(event.xmotion.y - center.y, scale);
+						XClearArea(display, window, 0, 0, width, height, True);
+					}else{
+						// 白マス/黒マス切り替え　ではなく　移動
+						if(RNG(pressed.x, event.xmotion.x) + RNG(pressed.y, event.xmotion.y) > 2){
+							center.x = event.xmotion.x - pressed.x;
+							center.y = event.xmotion.y - pressed.y;
+							no_move = False;
+							XClearArea(display, window, 0, 0, width, height, True);
+						}
+					}
+				}else if(select_mode == pasting){
+					// 貼り付け中に動いた
+					pasting_pointer.i = DIV(event.xmotion.x - center.x, scale);
+					pasting_pointer.j = DIV(event.xmotion.y - center.y, scale);
 					XClearArea(display, window, 0, 0, width, height, True);
 				}
 				break;
 			case ButtonRelease:
 				if(event.xbutton.button == Button1){
-					if(copying){
-						copying = False;
-					}else{
-						if(no_move){
-							int i = pressed.x;
-							int j = pressed.y;
-							if(i >= 0) i /= scale;
-							else i = -(-i - 1)/scale - 1;
-							if(j >= 0) j /= scale;
-							else j = -(-j - 1)/scale - 1;
-							if(in(i + N / 2, j + N / 2)) set[i + N / 2][j + N / 2] ^= 1;
-							XClearArea(display, window, center.x + i * scale, center.y + j * scale, scale, scale, True);
-						}
-					}
-					button1pressed = False;
-				}else if(event.xbutton.button == Button3){
-					copy_area.x2 = event.xbutton.x;
-					copy_area.y2 = event.xbutton.y;
-					if(copy_area.x2 >= 0) copy_area.x2 /= scale;
-					else copy_area.x2 = -(-copy_area.x2 - 1)/scale - 1;
-					if(copy_area.y2 >= 0) copy_area.y2 /= scale;
-					else copy_area.y2 = -(-copy_area.y2 - 1)/scale - 1;
-					if(copy_area.x1 > copy_area.x2){
-						int tmp = copy_area.x1;
-						copy_area.x1 = copy_area.x2;
-						copy_area.x2 = tmp;
-					}
-					if(copy_area.y1 > copy_area.y2){
-						int tmp = copy_area.y1;
-						copy_area.y1 = copy_area.y2;
-						copy_area.y2 = tmp;
-					}
-					for(int i = copy_area.x1; i <= copy_area.x2; ++i){
-						for(int j = copy_area.y1; j <= copy_area.y2; ++j){
-							if(in(i + N / 2, j + N / 2)){
-								copy[i + N / 2][j + N / 2] = set[i + N / 2][j + N / 2];
+					if(button1pressed){
+						if(select_mode == selecting){
+							select_mode = selected;
+						}else if(no_move){
+							if(select_mode == pasting){
+								// 貼り付け
+								for(int i = copy_area.i1; i < copy_area.i2; ++i)
+								for(int j = copy_area.j1; j < copy_area.j2; ++j){
+									set[pasting_pointer.i + i - copy_area.i1][pasting_pointer.j + j - copy_area.j1]
+									= copy[i][j];
+								}
+								select_mode = not_selecting;
+								XClearArea(display, window, 0, 0, width, height, True);
+							}else{
+								// 白マス/黒マス切り替え
+								int i = DIV(pressed.x, scale);
+								int j = DIV(pressed.y, scale);
+								if(INFIELD(i, j)) set[i][j] ^= 1;
+								XClearArea(display, window, center.x + i * scale, center.y + j * scale, scale, scale, True);
 							}
 						}
+						button1pressed = False;
+						XClearArea(display, window, 0, 0, width, height, True);
 					}
-					copying = True;
 				}
 				break;
 			case KeyPress:
 				switch(XLookupKeysym(&event.xkey, 0)){
+					char key;
+					case 'x':
+						key = 'x';
+					case 'c':
+						if(event.xkey.state & ControlMask){
+							if(select_mode == selected){
+								if(copy_area.i1 > copy_area.i2){
+									int tmp = copy_area.i1;
+									copy_area.i1 = copy_area.i2;
+									copy_area.i2 = tmp;
+								}
+								if(copy_area.j1 > copy_area.j2){
+									int tmp = copy_area.j1;
+									copy_area.j1 = copy_area.j2;
+									copy_area.j2 = tmp;
+								}
+								for(int i = copy_area.i1; i <= copy_area.i2; ++i)
+								for(int j = copy_area.j1; j <= copy_area.j2; ++j)
+								{
+									copy[i][j] = set[i][j];
+									if(key == 'x') set[i][j] = 0;
+								}
+								select_mode = not_selecting;
+								XClearArea(display, window, 0, 0, width, height, True);
+							}
+						}
+						break;
+					case 'v':
+						if(event.xkey.state & ControlMask){
+							select_mode = pasting;
+							XClearArea(display, window, 0, 0, width, height, True);
+						}
+						break;
 					case XK_Return:
 						goto run;
+						break;
 					case XK_Escape:
 						XDestroyWindow(display, window);
-						break;
-					case 's':
-						sleeptime *= 2;
-						break;
-					case 'f':
-						if(sleeptime > 1) sleeptime /= 2;
 						break;
 				}
 		}
@@ -235,6 +265,17 @@ run:
 				case KeyPress:
 					switch(XLookupKeysym(&event.xkey, 0)){
 						case XK_Return:
+							XSelectInput(
+								display,
+								window,
+								ExposureMask |
+								KeyPressMask |
+								ButtonPressMask |
+								PointerMotionMask |
+								ButtonReleaseMask |
+								StructureNotifyMask
+							);
+							XClearArea(display, window, 0, 0, width, height, True);
 							goto set;
 						case XK_Escape:
 							XDestroyWindow(display, window);
@@ -252,7 +293,7 @@ run:
 				static int eight[] = {-1, -1, 0, 1, -1, 1, 1, 0, -1};
 				count[i][j] = 0;
 				for(int k = 0; k < 8; ++k){
-					if(in(i + eight[k], j + eight[k + 1])) count[i][j] += field[i + eight[k]][j + eight[k + 1]];
+					if(INFIELD(i + eight[k], j + eight[k + 1])) count[i][j] += field[i + eight[k]][j + eight[k + 1]];
 				}
 			}
 			for(int i = 0; i < N; ++i) for(int j = 0; j < N; ++j){
@@ -273,11 +314,11 @@ run:
 					++j
 				){
 					int size = scale - (scale >= 10);
-					if(in(i + N / 2, j + N / 2))
+					if(INFIELD(i, j))
 						XFillRectangle(
 							display,
 							window,
-							field[i + N / 2][j + N / 2]?  whiteGC : blackGC,
+							field[i][j] ?  whiteGC : blackGC,
 							center.x + i * scale,
 							center.y + j * scale,
 							size,
